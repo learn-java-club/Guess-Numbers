e8a0b998a135e7813b76773f0685b36b5fa12899
sachiniattanayake <sachini.a@scale360solutions.com>
2019-06-11 10:38:37 +0700

    Refactored code, - Moved GameInstructions to another class. - Added Player and Util Class

--- src/Main.java
+++ src/Main.java
@@ -15,52 +15,53 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Guess the number!");
         Random rand = new Random();
-        // Sample StringBuffer
-        StringBuffer sbuffer = new StringBuffer();
-        String playAgain = "Y";
+
         String newGame = "Y";
         if (newGame == "Y") {
             do {
-                int counter = 0;
+
                 int guess = rand.nextInt(100);
                 guess += 1;
-                EndOfGame eog = new EndOfGame(); //In order to use printMessage method you need to create an instance of EndOfGame
 
                 int playersguess = 0;
 
                 Scanner input = new Scanner(System.in);
                 System.out.println("\nPlease enter your name: ");
-                String playersname = input.nextLine();
-                System.out.println("\nDear " + playersname + " have fun playing this game!");
-                System.out.println("\nTry to guess the number between 1 and 100.");
-                System.out.println("\nThe goal is to use as few guesses as possible. Train your brain!");
+
+                Player player = new Player(input.nextLine());
+                System.out.println(String.format(GameInstructions.welcomeMsg, player.playername));
+
 
                 do {
                     System.out.println("\nEnter your guess: ");
-                    playersguess = input.nextInt();
-                    counter++;
+                    String userInput = input.next();
+                    if (Util.isInteger(userInput, player) > 0) {
+                        playersguess = player.playerguess;
 
-                    if (playersguess <= 0 || playersguess > 100) {
-                        System.out.println("Invalid guess.");
-                        System.out.println("Your guess must be between 1 and 100!");
 
-                        counter = counter - 1;
+                        if (playersguess <= 0 || playersguess > 100) {
+                            System.out.println(GameInstructions.valueOutOfRange);
+                            player.playerguess--;
+                        } else if (playersguess < guess ) {
+                            System.out.println(GameInstructions.valueTooSmall);
+
+                        } else if (playersguess > guess )
+                            System.out.println(GameInstructions.valueTooBig);
                     }
-                    if (playersguess < guess & playersguess > 0) {
-                        System.out.println("Your guess is too small");
-                    } else if (playersguess > guess & playersguess < 101)
-                        System.out.println("Your guess is too big!");
+                    else {
+                        System.out.println(GameInstructions.notNumber);
+                    }
                 }
                 while (playersguess != guess);
-                System.out.println("\nWell done " + playersname + "! You guessed the secret number! Bravo!");
-                System.out.println("\nYou needed " + counter + " guesses.");
-                //eog.printMessage(); //then call the method
+                System.out.println(String.format(GameInstructions.winningMsg, player.playername, player.guesses));
+
 
                 Scanner inputPlayAgain = new Scanner(System.in);
-                System.out.println("\nDo you want to play again? Y or N: ");
+                System.out.println(GameInstructions.askForRematch);
                 newGame = inputPlayAgain.nextLine();
+
                 System.out.println("\nYou entered: " + newGame);
-                //playAgain = newGame;
+
 
             } while (newGame.equals("Y"));
         }
